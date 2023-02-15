4453d5adbf4c1ef64c3974c5cc5b971bab43c8ca
alipanahi <p.alihussain@gmail.com>
2023-02-15 19:48:30 +0100

    sidebar info on dblclic

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -11,19 +11,12 @@ import * as L from 'leaflet';
 })
 
 export class MapComponent implements OnInit {
-  legend_view:boolean = false;
   
   constructor(private layerService: LayersService) { }
 
   
   ngOnInit(): void {
-    ////////// on dblclick event /////////////////
-    function highlightFeature(e:any){
-      //this.legend_view = false;
-      let div:any = document.getElementById('dataDiv');
-      div.innerHTML=`<p>${e.latlng}</p>`;
-      //alert(e.latlng);
-    }
+    
     /////////////// pointers dblclick event ///////////////////
     function onEachFeature(feature: any, layer: any) {
       // does this feature have a property named popupContent?
@@ -31,31 +24,91 @@ export class MapComponent implements OnInit {
         layer.bindPopup(feature.properties.popupContent);
       }
       layer.on({
-        dblclick: highlightFeature
+        dblclick: showSidebarWidthText
       });
 
     }
+    function showSidebarWidthText(e:any) {
+      console.log(e);
+      document.querySelector(".map_lengend")?.classList.remove("hidden");
+      addContentToSidebar(e);
+
+    }
+    function addContentToSidebar(e:any) {
+    
+      // create sidebar content
+      const sidebarTemplate = `
+      <article class="sidebar-content">
+        <h3 style="position: sticky;top: 0;color: #fff;background: #0052b1;padding: 10px 35px 10px 15px;">
+          ${e.target.feature.properties.name}-<span>${e.latlng}</span>
+          </h3>
+        <div class="info-content">
+          <div class="info-description"><p>${e.target.feature.properties.amenity}</p></div>
+        </div>
+      </article>
+  
+      `;
+    
+      const sidebar = document.querySelector(".map_lengend");
+      const sidebarContent = document.querySelector(".sidebar-content");
+    
+      // always remove content before adding new one
+      sidebarContent?.remove();
+    
+      // add content to sidebar
+      sidebar?.insertAdjacentHTML("beforeend", sidebarTemplate);
+    
+    }
+    // close when click esc
+    document.addEventListener("keydown", function (event) {
+      // close sidebar when press esc
+      if (event.key === "Escape") {
+        closeSidebar();
+      }
+    });
+    const buttonClose = document.querySelector(".close-button");
+    buttonClose?.addEventListener("click", () => {
+      // close sidebar when click on close button
+      closeSidebar();
+    });
+    function closeSidebar() {
+      // remove class active-sidebar
+      document.querySelector(".map_lengend")?.classList.add("hidden");
+    }
+
+
 
     let map = L.map('map',{fullscreenControl: true}).setView([44.414165, 8.942184], 5);
-    
-    //markers
-    var marker_genoa = L.marker([44.4, 8.9]).addTo(map).bindPopup("<b>Genoa!</b><br>ETT S.p.A.<br>Double click to see the coordination!");
-    var marker_manduria = L.marker([40.40221612940531, 17.632182690036974]).addTo(map);
-    //marker_manduria.bindPopup("<b>Manduria!</b><br>Ali Hussain").openPopup();
-    marker_manduria.bindPopup("<b>Manduria!</b><br>Ali Hussain");
-    
-
-    //double click event listener
-    marker_genoa.on('dblclick', highlightFeature);
-    
     
     /////////////////// manual data  ///////////////////
     var points = [{
       "type": "Feature",
       "properties": {
-        "name": "rome pointer",
-        "amenity": "pointer",
-        "popupContent": "Roma!<br>Double click to see the coordination!"
+        "name": "Genoa",
+        "amenity": "Genoa (Genova) is a port city and the capital of northwest Italy's Liguria region. It's known for its central role in maritime trade over many centuries.",
+        "popupContent": "<b>Genoa!</b><br>Double click to see the details!"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [8.9, 44.4]
+      }
+    },{
+      "type": "Feature",
+      "properties": {
+        "name": "Manduria",
+        "amenity": "Manduria is a city and comune of Apulia, Italy, in the province of Taranto. With c. 32,000 inhabitants, it is located 35 kilometres east of Taranto. ",
+        "popupContent": "<b>Manduria!</b><br>Double click to see the details!"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [17.632182690036974, 40.40221612940531]
+      }
+    }, {
+      "type": "Feature",
+      "properties": {
+        "name": "Rome",
+        "amenity": "Rome is the capital city of Italy. It is also the capital of the Lazio region, the centre of the Metropolitan City of Rome, and a special comune named Comune di Roma Capitale.",
+        "popupContent": "<b>Rome!</b><br>Double click to see the details!"
       },
       "geometry": {
         "type": "Point",
@@ -66,7 +119,7 @@ export class MapComponent implements OnInit {
       "properties": {
         "name": "genoa",
         "amenity": "path to Genoa",
-        "popupContent": "<b>fastes path to Genoa!</b><br>Double click to see the coordination!"
+        "popupContent": "<b>fastes path to Genoa!</b><br>Double click to see the details!"
       },
       "geometry": {
         "type": "LineString",
@@ -81,7 +134,7 @@ export class MapComponent implements OnInit {
       "properties": {
         "name": "roma",
         "amenity": "Path to roma",
-        "popupContent": "<b>This is the path to roma!</b><br>Double click to see the coordination!"
+        "popupContent": "<b>This is the path to roma!</b><br>Double click to see the details!"
       },
       "geometry": {
         "type": "LineString",
@@ -101,8 +154,11 @@ export class MapComponent implements OnInit {
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
     ////////////////// wms layer     //////////////
-    var wmsLayer = L.tileLayer.wms('http://ows.mundialis.de/services/service?', {
+    var wmsLayer = L.tileLayer.wms('https://ows.mundialis.de/osm/service?', {
       layers: 'TOPO-OSM-WMS'
+    });
+    var Floods = L.tileLayer.wms('http://wms.pcn.minambiente.it/ogc?',{
+      layers: 'Sicilia_ITH2018'
     });
     ////////////////  earth at night layer       ////////////
     var earthatnight = L.tileLayer('https://map1.vis.earthdata.nasa.gov/wmts-webmerc/VIIRS_CityLights_2012/default/{time}/{tilematrixset}{maxZoom}/{z}/{y}/{x}.{format}', {
@@ -143,7 +199,8 @@ export class MapComponent implements OnInit {
       var baseMaps = {
         "OpenStreetMap": myLayer,
         "EarthAtNight": earthatnight,
-        "wms": wmsLayer
+        "wms": wmsLayer,
+        "Floods": Floods
       };
       var overlayMaps = {
         "Paths": geoData,
