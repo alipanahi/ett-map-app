d5dc4dbcdf1d5c73a63428912b3d062e4864aed4
alipanahi <p.alihussain@gmail.com>
2023-02-17 22:30:57 +0100

    custom layer controller

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { LayersService } from '../layers.service';
+//import { LayersService } from '../layers.service';
 // @ts-ignore
 import * as L from 'leaflet';
 
@@ -12,11 +12,71 @@ import * as L from 'leaflet';
 
 export class MapComponent implements OnInit {
 
-  constructor(private layerService: LayersService) { }
+  constructor() { }
+
+  private map:any;
+  
+  private geoData: any;
+  private paths:any;
+
+  public layerArray:any;
+  private layerList:any=fetch('/assets/layers.json')
+    .then((response) => response.json())
+    .then((data) => this.layerArray=data);
+  
+  private initMap():void{
+    this.map = L.map('map', { fullscreenControl: true }).setView([44.414165, 8.942184], 5);
+  }
+  // function to set layer base on user click
+  public setLayer(layer:string='default'): void {console.log(layer);
+    if(layer=='default'){
+      ///////////////// default layer /////////////////
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        minZoom: 3,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      }).addTo(this.map);
+    }else{
+      
+        let selectedLayer = this.layerArray.filter((item:any)=>item.name==layer);
+        //console.log(selectedLayer[0].url);
+        L.tileLayer.wms(selectedLayer[0].url, {
+          layers: selectedLayer[0].layers
+        }).addTo(this.map);
+    
+    }
+  }
+  public showMarkers(e:any,marker:string):void{
+  
+    if(e.target.checked){
+      if(marker=='pointers'){
+        this.geoData.addTo(this.map);
+      }else if(marker=='paths'){
+        this.paths.addTo(this.map);
+      }
+    }else{
+      if(marker=='pointers'){
+        this.map.removeLayer(this.geoData);
+      }else if(marker=='paths'){
+        this.map.removeLayer(this.paths);
+      }
+    }
+  }
+  // async function to get data from json
+  async fetchData(url: any) {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
 
   ngOnInit(): void {
-
+    this.initMap();
+    
     /////////////// pointers dblclick event ///////////////////
     function onEachFeature(feature: any, layer: any) {
       // does this feature have a property named popupContent?
@@ -32,7 +92,7 @@ export class MapComponent implements OnInit {
       //console.log(e.latlng.lat);
       document.querySelector(".map_lengend")?.classList.remove("hidden");
       addContentToSidebar(e);
-
+  
     }
     function addContentToSidebar(e: any) {
       // create sidebar content
@@ -45,16 +105,16 @@ export class MapComponent implements OnInit {
           <div class="info-description"><p>${e.target.feature.properties.amenity}</p></div>
         </div>
       </article>`;
-
+  
       const sidebar = document.querySelector(".map_lengend");
       const sidebarContent = document.querySelector(".sidebar-content");
-
+  
       // always remove content before adding new one
       sidebarContent?.remove();
-
+  
       // add content to sidebar
       sidebar?.insertAdjacentHTML("beforeend", sidebarTemplate);
-
+  
     }
     // close when click esc
     document.addEventListener("keydown", function (event) {
@@ -72,25 +132,18 @@ export class MapComponent implements OnInit {
       // remove class active-sidebar
       document.querySelector(".map_lengend")?.classList.add("hidden");
     }
-    // async function to get data from json
-    async function fetchData(url: any) {
-      try {
-        const response = await fetch(url);
-        const data = await response.json();
-        return data;
-      } catch (err) {
-        console.error(err);
-      }
-    }
-
-
-    let map = L.map('map', { fullscreenControl: true }).setView([44.414165, 8.942184], 5);
-
+    //this.map.on("click", addLayer);
     /////////////////// pointers data from json file  ///////////////////
-    let geoData: any;
-    fetchData('/assets/pointers.json')
+    this.fetchData('/assets/pointers.json')
       .then((data) => {
-        geoData = L.geoJSON(data, {
+        this.geoData = L.geoJSON(data, {
+          onEachFeature: onEachFeature
+        }).addTo(this.map);
+        
+      });
+    this.fetchData('/assets/paths.json')
+      .then((data) => {
+        this.paths = L.geoJSON(data, {
           style: function (feature: any): any {
             switch (feature.properties.name) {
               case 'genoa': return { color: "#ff0000", weight: 4, opacity: 0.5 };
@@ -98,30 +151,13 @@ export class MapComponent implements OnInit {
             }
           },
           onEachFeature: onEachFeature
-        }).addTo(map);
+        }).addTo(this.map);
+        
       });
-    ///////////////// OpenStreetMap layer /////////////////
-    let myLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      minZoom: 3,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
-    ////////////////// wms layer     //////////////
-    var wmsLayer = L.tileLayer.wms('https://ows.mundialis.de/osm/service?', {
-      layers: 'TOPO-OSM-WMS'
-    });
-    //layer from www.pcn.minambiente.it
-    var Floods = L.tileLayer.wms('http://wms.pcn.minambiente.it/ogc?map=/ms_ogc/WMS_v1.3/Vettoriali/Batimetrica.map', {
-      layers: 'EL.BATIMETRICA.'
-    });
-    var predefinito = L.tileLayer.wms('http://wms.pcn.minambiente.it/ogc?map=/ms_ogc/WMS_v1.3/Vettoriali/Bacini_idrografici.map', {
-      layers: 'ID.ACQUEFISICHE.BACINIIDROGRAFICI.PRINCIPALI'
-    });
-    var ecopedologica = L.tileLayer.wms('http://wms.pcn.minambiente.it/ogc?map=/ms_ogc/WMS_v1.3/Vettoriali/Carta_ecopedologica.map', {
-      layers: 'GE.CARTAECOPEDOLOGICA.GENERALE'
-    });
+    this.setLayer();
+    /*
     /////////////////////////   points style        /////////////
-
+    
     var stateStyle = {
       "color": "#008f68",
       "weight": 3,
@@ -134,23 +170,11 @@ export class MapComponent implements OnInit {
     this.layerService.getStateShapes().subscribe(states => {
       usaData = L.geoJSON(states, {
         style: stateStyle
-      }).addTo(map);
+      }).addTo(this.map);
 
-      ///////////////////////    layers control    ////////////////////
-      var baseMaps = {
-        "OpenStreetMap": myLayer,
-        "wms": wmsLayer,
-        "Floods": Floods,
-        "predefinito":predefinito,
-        "Carta ecopedologica d'Italia":ecopedologica
-      };
-      var overlayMaps = {
-        "Paths": geoData,
-        "USA States": usaData,
-      };
-      L.control.layers(baseMaps, overlayMaps, { collapsed: false }).addTo(map);
+      
     });
-
+    */
 
   }
 
